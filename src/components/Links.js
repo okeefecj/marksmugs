1569ed82d242f42b127f5bc0363314c167fe28a0
nathandravo <101357077+nathandravo@users.noreply.github.com>
2022-09-28 18:16:04 -0400

    Add files via upload

---
+++ src/components/Links.js
@@ -0,0 +1,28 @@
+import Starbucks from "./Starbucks.png";
+import Target from "./Target.jpg";
+import CircuitCity from "./CircuitCity.png";
+import BassProShops from "./BassProShops.png";
+
+const Links = () => {
+    return (  
+        <div className="Links">
+            <h2>Our Retailers</h2> 
+                <body>
+                    <a href="https://www.starbucks.com/">
+                    <img src={Starbucks} width="150" height="150"/>
+                    </a>
+                    <a href="https://www.target.com/">
+                        <img src={Target} width="150" height="150"/>
+                    </a>
+                    <a href="https://circuitcity.com/"> 
+                        <img src={CircuitCity} width="150" height="150"/>
+                    </a>
+                    <a href="https://www.basspro.com/shop/en#">
+                        <img src={BassProShops} width="150" height="150"/>
+                    </a>
+                </body>
+            </div>
+    );
+}
+ 
+export default Links;
