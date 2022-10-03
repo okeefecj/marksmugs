0eeeb2e963249f0a87071e283c415840a6b72d92
nathandravo <101357077+nathandravo@users.noreply.github.com>
2022-10-03 17:50:55 -0400

    Add files via upload

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,26 +3,29 @@ import Target from "./Target.jpg";
 import CircuitCity from "./CircuitCity.png";
 import BassProShops from "./BassProShops.png";
 
+const imagePaddingStyle = {
+    padding: '12.5px'
+}
+
 const Links = () => {
     return (  
         <div className="Links">
-            <h2>Our Retailers</h2> 
+            <h2>Our Vendors</h2> 
                 <body>
                     <a href="https://www.starbucks.com/">
-                    <img src={Starbucks} width="150" height="150"/>
+                    <img src={Starbucks} width="150" height="150" style={imagePaddingStyle}/>
                     </a>
                     <a href="https://www.target.com/">
-                        <img src={Target} width="150" height="150"/>
+                        <img src={Target} width="150" height="150" style={imagePaddingStyle}/>
                     </a>
                     <a href="https://circuitcity.com/"> 
-                        <img src={CircuitCity} width="150" height="150"/>
+                        <img src={CircuitCity} width="150" height="150" style={imagePaddingStyle}/>
                     </a>
                     <a href="https://www.basspro.com/shop/en#">
-                        <img src={BassProShops} width="150" height="150"/>
+                        <img src={BassProShops} width="150" height="150" style={imagePaddingStyle}/>
                     </a>
                 </body>
             </div>
     );
 }
- 
 export default Links;
